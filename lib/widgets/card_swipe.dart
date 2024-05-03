8fb39004f3e3e23246f24c9e1358d4094f2cdb60
jrms <email@fake.com>
2024-05-03 12:45:49 -0700

    Add Slider Carrousel images widget

--- lib/widgets/card_swipe.dart
+++ lib/widgets/card_swipe.dart
@@ -1,14 +1,36 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class CardSwipe extends StatelessWidget {
   const CardSwipe({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Container(
       width: double.infinity,
-      height: 500,
-      color: Colors.red,
+      height: size.height * 0.5,
+      child: Swiper(
+          itemCount: 10,
+          layout: SwiperLayout.STACK,
+          itemWidth: size.width * 0.6,
+          itemHeight: size.height * 0.4,
+          itemBuilder: (_, int index) {
+            return GestureDetector(
+              onTap: () => Navigator.pushNamed(context, 'details',
+                  arguments: 'movie-instance'),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                    placeholder: const AssetImage('assets/img/loading.gif'),
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        'https://picsum.photos/id/$index/300/400')),
+              ),
+            );
+          }),
     );
   }
 }
