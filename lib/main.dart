ae86996be29cd652dd469b88d173eb54e93d33a9
jrms <email@fake.com>
2024-05-04 13:51:15 -0700

    Add AppState widget with Multiprovider to wrap MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:movies_app/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
 import 'screens/screens.dart';
 
-void main() => runApp(const MyApp());
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MoviesProvider(), lazy: false),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
+
+void main() => runApp(const AppState());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,4 +1,6 @@
-class MoviesProvider {
+import 'package:flutter/material.dart';
+
+class MoviesProvider extends ChangeNotifier {
   MoviesProvider() {
     print('MoviesProvider');
     this.getNowPlayingMovies();
