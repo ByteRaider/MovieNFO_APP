71e533a891d61fcf63009dfb33327f9d417a405b
jrms <email@fake.com>
2024-05-04 09:11:59 -0700

    Add MovieDetails SliverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,9 +5,48 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text('Details Screen'),
-    ));
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+    return Scaffold(
+        body: CustomScrollView(slivers: [
+      _CustomAppBar(),
+    ]));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.all(0),
+        centerTitle: true,
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          padding: const EdgeInsets.only(bottom: 10),
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(
+                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+            textAlign: TextAlign.center,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/img/loading.gif'),
+          image: NetworkImage('https://picsum.photos/500/300'),
+          fit: BoxFit.cover,
+          fadeInDuration: Duration(milliseconds: 500),
+          fadeInCurve: Curves.easeIn,
+        ),
+      ),
+    );
   }
 }
