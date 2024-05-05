f7c99a03329355e52c6c57444caa14e0701bbfbf
jrms <email@fake.com>
2024-05-04 17:26:38 -0700

    Map http request

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:movies_app/models/now_playing_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   final String _apiKey = '7b433b7afe7c810996ff37e1804bb68d';
@@ -18,8 +19,7 @@ class MoviesProvider extends ChangeNotifier {
     var url = Uri.https(_baseUrl, '/3/movie/now_playing',
         {'api_key': _apiKey, 'language': _language, 'page': '1'});
     final response = await http.get(url);
-    if (response.statusCode > 300) return (response.statusCode);
-    final Map<String, dynamic> decodedData = jsonDecode(response.body);
-    print(decodedData);
+    final nowPlayingResponse =
+        NowPlayingResponse.fromJson(jsonDecode(response.body));
   }
 }
