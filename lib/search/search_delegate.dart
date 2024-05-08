ab46350b50b7e1aed4bc398ec3a8831c6940d57a
jrms <email@fake.com>
2024-05-07 21:47:44 -0700

    Show movie item based on query

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/models/models.dart';
+import 'package:movies_app/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   @override
@@ -34,8 +37,38 @@ class MovieSearchDelegate extends SearchDelegate {
           child: Icon(Icons.movie_creation_outlined, size: 130),
         ),
       );
-    } else {
-      return Container();
     }
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+    return FutureBuilder(
+        future: moviesProvider.searchMovies(query),
+        builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
+          if (!snapshot.hasData) {
+            return Container();
+          }
+          final movies = snapshot.data!;
+          return ListView.builder(
+            itemCount: movies.length,
+            itemBuilder: (_, int index) => _MovieItem(movies[index]),
+          );
+        });
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  const _MovieItem(this.movie);
+  final Movie movie;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FadeInImage(
+        placeholder: const AssetImage('assets/img/noImage.jpg'),
+        image: NetworkImage(movie.getFullPosterImg()),
+        width: 50,
+        fit: BoxFit.contain,
+      ),
+      title: Text(movie.title),
+      subtitle: Text(movie.originalTitle),
+      onTap: () => Navigator.pushNamed(context, 'details', arguments: movie),
+    );
   }
 }
